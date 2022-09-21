06d0e25b71c145425752880963c610627503298b
Paulo Barata <pbara7a@gmail.com>
2022-09-21 11:47:54 +0100

    Calculator operation functions.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// calculator functions
+const add = (a, b) => a + b;
 
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const operations = {
+  "+": add,
+  "-": subtract,
+  x: multiply,
+  "/": divide,
+};
+
+const operate = (operator, a, b) => operations[operator](a, b);
