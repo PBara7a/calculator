06c5c0db2fb7478a784fe16229709432d419ea55
Paulo Barata <pbara7a@gmail.com>
2022-09-22 12:58:36 +0100

    Add, subtract, divide and multiply functionality.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// calculator functions
+const mainScreen = document.querySelector("#screen-bottom");
+const topScreen = document.querySelector("#screen-top");
+const numBtns = document.querySelectorAll(".num");
+const allClearBtn = document.querySelector("#ac");
+const operationBtns = document.querySelectorAll(".operation");
+const equalsBtn = document.querySelector("#equals");
+
+// Calculator functions
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -15,3 +22,81 @@ const operations = {
 };
 
 const operate = (operator, a, b) => operations[operator](a, b);
+
+let currentOnDisplay = mainScreen.textContent;
+let firstOperand;
+let currentOperation;
+let needReset = false;
+
+const appendOnDisplay = (num) => {
+  if (needReset) {
+    resetMainScreen();
+  }
+
+  if (currentOnDisplay === "0") {
+    mainScreen.textContent = num;
+  } else if (currentOnDisplay.length < 14) {
+    // screen max -> 14 chars
+    mainScreen.textContent += num;
+  }
+  currentOnDisplay = mainScreen.textContent;
+  needReset = false;
+};
+
+const setOperation = (operation) => {
+  if (firstOperand) {
+    firstOperand = operate(
+      currentOperation,
+      firstOperand,
+      Number(currentOnDisplay)
+    );
+  } else {
+    firstOperand = Number(currentOnDisplay);
+  }
+  currentOperation = operation;
+  needReset = true;
+
+  updateTopScreen();
+};
+
+const updateTopScreen = () => {
+  topScreen.textContent = `${firstOperand} ${currentOperation}`;
+};
+
+const resetMainScreen = () => {
+  mainScreen.textContent = "0";
+  currentOnDisplay = "0";
+};
+
+const allClear = () => {
+  resetMainScreen();
+  topScreen.textContent = "";
+  firstOperand = "";
+  currentOperation = "";
+};
+
+const handleEquals = () => {
+  const result = operate(
+    currentOperation,
+    firstOperand,
+    Number(currentOnDisplay)
+  );
+
+  allClear();
+
+  appendOnDisplay(result);
+  needReset = true;
+};
+
+// Setup events
+numBtns.forEach((btn) => {
+  btn.onclick = () => appendOnDisplay(btn.textContent);
+});
+
+operationBtns.forEach((btn) => {
+  btn.onclick = () => setOperation(btn.textContent);
+});
+
+allClearBtn.onclick = allClear;
+
+equalsBtn.onclick = handleEquals;
